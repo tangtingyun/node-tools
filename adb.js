c138b8cfcff16173578df7a8f707ed3aa4ded942
wanger <958019947@qq.com>
2022-06-09 21:59:50 +0800

    mac

--- adb.js
+++ adb.js
@@ -2,11 +2,18 @@ const fs = require('fs')
 const path = require('path')
 const { commandSpawn } = require('./sample/utils/terminal');
 
+
+var uiPath = path.resolve(__dirname, "ui.xml");
+if (fs.existsSync(uiPath)) {
+    fs.rmSync(uiPath);
+}
+
 // adb shell screencap -p /sdcard/screen2.png
 // adb pull /sdcard/screen1.png
 
 // adb shell uiautomator dump /sdcard/ui.xml
 commandSpawn("adb", ["shell", "uiautomator", "dump", "/sdcard/ui.xml"])
-
-// adb pull /sdcard/ui.xml 
-commandSpawn("adb", ["pull", "/sdcard/ui.xml"])
+    .then(() => {
+        // adb pull /sdcard/ui.xml 
+        commandSpawn("adb", ["pull", "/sdcard/ui.xml"])
+    })
